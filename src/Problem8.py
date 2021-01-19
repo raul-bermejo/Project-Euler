49ba3f35096ba5389fd1591c086cdff406492ae0
raul-bermejo <raulbv.personal@gmail.com>
2021-01-20 08:57:53 +1300

    Didn't solve Problem 8 yet, however I am working on a general solution for any number N.

--- src/Problem8.py
+++ src/Problem8.py
@@ -14,6 +14,7 @@ greatest product. What is the value of this product?
 """
 
 import time
+import re
 
 start = time.time()
 
@@ -32,6 +33,9 @@ def stringProd(s):
           Product of these elements
     """
     
+    s = re.sub(r"\s", "", s)    # Remove all whitespace
+    s = re.sub(r"\D.*", "", s)  # Remove everything starting at a non-digit
+    
     p = 1
     lst = [int(s[i]) for i in range(len(s))]
     for num in lst:
@@ -39,7 +43,7 @@ def stringProd(s):
 
     return p
 
-def find_adjacent_GP(n):
+def find_adjacent_GP(s, n):
     """
     Finds the n adjacent digits in an str input number s.t. they have the 
     greatest product.
@@ -57,8 +61,23 @@ def find_adjacent_GP(n):
         Greatest product of all possible adjacent products
     """
     
+    # Initialize variables
+    size = len(s)
+    start, end = 0, n
+    currProd = 0
+    prodLst = []
     
-    return None
+    # Loop over the input string to compute the product of 13 adjacents
+    while (end < size-1):
+        newProd = stringProd(s[start:end])
+        if newProd > currProd:
+            prodLst.append((newProd, start, end))
+        # Increase counters
+        start += 1; end += 1
+        
+    # Then find the maximum 
+    
+    return prodLst
     
 
 largeNum = """73167176531330624919225119674426574742355349194934 
@@ -83,6 +102,9 @@ largeNum = """73167176531330624919225119674426574742355349194934
 71636269561882670428252483600823257530420752963450"""
 
 
+N = 13                                                      # # of digits stated in the problem
+lst = find_adjacent_GP(largeNum, N)
+print(max(lst))
 
 end = time.time()
 print(f"The program took {end-start:.4e} to run.")
